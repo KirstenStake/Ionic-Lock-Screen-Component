c78a0635f6d4cd4163f988d33a4624b01a69a287
Kirsten Frager <kirsten@hellostake.com>
2020-12-23 18:11:57 +1100

    ref: UI redesign

--- src/components/lock-screen-component.ts
+++ src/components/lock-screen-component.ts
@@ -1,15 +1,11 @@
-/**
- * [Auto Generated] Created by :Carson. Chen
- */
+import { NavParams, NavController, Events } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { Events, NavParams, NavController } from 'ionic-angular';
 import { Md5 } from 'ts-md5/dist/md5';
-
 
 /* HTML Template */
 const LOCK_SCREEN_TEMPLATE = `
       <div class="ILS_lock" [ngClass]="!_showLockScreen ?  'ILS_lock-hidden' : ''">
-        <div class="ILS_label-row">
+        <div class="ILS_label-row ISL_label_title">
           {{ !firstPasswordSet ? passcodeLabel : secondPasscodeLabel }}
         </div>
         <div *ngIf="passcodeSubTitle" class="ILS_label-row ISL_label_subTitle">
@@ -37,12 +33,11 @@ const LOCK_SCREEN_TEMPLATE = `
           <div (click)="digit(9)" class="ILS_digit">9</div>
         </div>
         <div class="ILS_numbers-row">
-          <div *ngIf="ACDelbuttons" (click)="allClear()" class="ILS_digit ILS_ac">AC</div>
+          <div *ngIf="onAdditionalLink" (click)="additionalLink()" class="ILS_digit ISL_label_link">
+          {{ additionalLinkTitle }}
+          </div>
           <div (click)="digit(0)" class="ILS_digit">0</div>
-          <div *ngIf="ACDelbuttons" (click)="remove()" class="ILS_digit ILS_del">DEL</div>
-        </div>
-        <div *ngIf="onAdditionalLink" class="ILS_label-row ISL_label_forgot">
-            <a (click)="additionalLink()">{{additionalLinkTitle}}</a>
+          <div *ngIf="ACDelbuttons" (click)="remove()" class="ILS_digit ILS_del"></div>
         </div>
       </div>
     `;
@@ -63,22 +58,23 @@ const LOCK_SCREEN_STYLE = `
           }
           @keyframes ILS_buttonPress {
             0% {
-              background-color: #E0E0E0;
+              background-color: #acafb2;
             }
             100% {
-              background-color: #F1F1F1;
+              background-color: #f9f9f9;
             }
           }
           /* Lock Screen Layout*/
           .ILS_lock {
             display: flex;
             flex-direction: column;
-            justify-content: center;
+            justify-content: flex-end;
             position: absolute;
             width: 100%;
             height: 100%;
             z-index: 999;
-            background-color: #F1F1F1;
+            padding: 45px 0;
+            background-color: #f9f9f9;
           }
           .ILS_lock-hidden {
             display: none;
@@ -87,23 +83,87 @@ const LOCK_SCREEN_STYLE = `
             height: 50px;
             width: 100%;
             text-align: center;
-            font-size: 23px;
             padding-top: 10px;
-            color: #464646;
+            color: #141414;
           }
+          .ISL_label_title {
+            font-size: 34px;
+            // margin-bottom: 0.5em;
+            text-transform: uppercase;
+          }
+          .ISL_label_subTitle {
+            font-size: 14px;
+            padding-top: 0;
+          }
+          .ISL_label_link {
+            color: #666666 !important;
+            font-size: 14px !important;
+            padding-top: 26px !important;
+            background-color: transparent !important;
+          }
+          .ILS_del {
+            padding: 0 !important;
+            margin: 25px 48px !important;
+            box-sizing: border-box;
+            position: relative;
+            display: block;
+            width: 14px !important;
+            height: 14px !important;
+            transform: scale(var(--ggs,1));
+            border: 2px solid;
+            border-left: 0;
+            color: #666666 !important;
+            border-radius: 0px !important;
+            border-top-right-radius: 2px !important;
+            background-color: transparent !important;
+            border-bottom-right-radius: 2px !important;
+          }
+            .ILS_del::after,
+            .ILS_del::before {
+            content: "";
+            display: block;
+            box-sizing: border-box;
+            position: absolute
+            }
+           
+            .ILS_del::before {
+            background:
+            linear-gradient( to left,
+            currentColor 18px,transparent 0)
+            no-repeat center center/10px 2px;
+            border-right: 3px solid transparent;
+            box-shadow: inset 0 0 0 2px;
+            right: 2px;
+            bottom: 1px;
+            width: 8px;
+            height: 8px;
+            border-left: 3px solid transparent;
+            transform: rotate(45deg)
+            }
+           
+            .ILS_del::after {
+            width: 10px;
+            height: 10px;
+            border-top: 2px solid;
+            border-left: 2px solid;
+            border-top-left-radius: 1px;
+            transform: rotate(-45deg);
+            top: 0;
+            left: -5px
+            } 
           .ILS_circles-row {
             display: flex;
             flex-direction: row;
             justify-content: center;
             width: 100%;
-            height: 60px;
+            height: 200px;
           }
           .ILS_circle {
-            background-color: #F1F1F1!important;
+            background-color: transparent;
             border-radius: 50%;
-            width: 10px;
-            height: 10px;
-            border:solid 1px #464646;
+            width: 12px;
+            height: 12px;
+            border: solid 1.5px #141414;
             margin: 0 15px;
           }
           .ILS_numbers-row {
@@ -111,18 +171,18 @@ const LOCK_SCREEN_STYLE = `
             flex-direction: row;
             justify-content: center;
             width: 100%;
-            height: 100px;
+            height: 85px;
           }
           .ILS_digit {
-            margin: 0 14px;
-            width: 80px;
-            border-radius: 10%;
-            height: 80px;
+            margin: 0 20px;
+            width: 68px;
+            border-radius: 50%;
+            height: 68px;
             text-align: center;
-            padding-top: 29px;
-            font-size: 21px;
-            color: #464646;
-            background-color: #bed7ef;
+            padding-top: 22px;
+            font-size: 25px;
+            color: #141414;
+            background-color: #eaebeb;
           }
           .ILS_digit.activated {
             -webkit-animation-name: ILS_buttonPress;
@@ -130,16 +190,8 @@ const LOCK_SCREEN_STYLE = `
             -webkit-animation-duration: 0.3;
             animation-duration: 0.3s;
           }
-          .ILS_ac {
-            color: #464646;
-            background-color: #F8F8F8;
-            }
-          .ILS_del {
-            color: #464646;
-            background-color: #F8F8F8;
-            }
           .ILS_full {
-            background-color:#464646!important;
+            background-color:#141414!important;
           }
           .ILS_shake {
             -webkit-animation-name: ILS_shake;
@@ -149,6 +201,7 @@ const LOCK_SCREEN_STYLE = `
             -webkit-animation-fill-mode: both;
             animation-fill-mode: both;
           }
+          
 `;
 
 @Component({
@@ -158,39 +211,41 @@ const LOCK_SCREEN_STYLE = `
 })
 export class LockScreenComponent {
 
-    _showLockScreen: boolean;
+    touchId: boolean;
+    setPasscode: boolean;       // initiate initial set passcode journey
     ACDelbuttons: boolean;
     passcodeWrong: boolean;
-    setPasscode: boolean;
-    touchId: boolean;
+    _showLockScreen: boolean;
     firstPasswordSet: boolean;
+    _passcodeFallBack: boolean;      // use pin page if biometrics fail
 
     passcodeAttempts: number = 0;
 
     passcode: string;
-    passcodeLabel: string;
-    secondPasscodeLabel: string;
-    passcodeSubTitle: string;
-    additionalLinkTitle: string;
     touchLabel: string;
+    passcodeLabel: string;
+    passcodeSubTitle: string;
     _firstSetPasscode: string;
+    additionalLinkTitle: string;
+    secondPasscodeLabel: string;
 
-    onCorrect: any;
     onWrong: any;
     selected: any;
+    onCorrect: any;
     onAdditionalLink: any;
     enteredPasscode: any = '';
 
     constructor(
         public events: Events,
+        private navParams: NavParams,
         private navCtrl: NavController,
-        private navParams: NavParams
     ) {
         this._showLockScreen = true;
         this.passcode = navParams.data.code;
         this.touchId = navParams.data.touchId || false;
         this.setPasscode = navParams.data.setPasscode || false;
         this.ACDelbuttons = navParams.data.ACDelbuttons || false;
+        this._passcodeFallBack = navParams.data.pinFallBack || false;
 
         this.onWrong = navParams.data.onWrong || null;
         this.onCorrect = navParams.data.onCorrect || null;
@@ -207,7 +262,7 @@ export class LockScreenComponent {
         this.onAdditionalLink && this.onAdditionalLink();
     }
 
-    allClear(): void {
+    _allClear(): void {
         this.enteredPasscode = "";
     }
 
@@ -247,12 +302,13 @@ export class LockScreenComponent {
                 if (!this.firstPasswordSet) {
                     this._firstSetPasscode = this.enteredPasscode;
                     this.firstPasswordSet = true;
-                    this.allClear();
+                    this._allClear();
                 } else {
                     if (this.enteredPasscode === this._firstSetPasscode) {
-                        this.onCorrect && this.onCorrect(this.enteredPasscode);
+                        this.onCorrect && this.onCorrect(Md5.hashStr(this.enteredPasscode));
                         this.enteredPasscode = '';
                         this.passcodeAttempts = 0;
+                        this._showLockScreen = false;
                         this._showLockScreen = false;
                         this.navCtrl.pop();
                     } else {
